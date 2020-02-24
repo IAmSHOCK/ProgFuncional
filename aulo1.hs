665147b21d665745398e2a07561c7ba2e994c13b
AiresDesktop <joaoaires98@gmail.com>
2020-02-24 16:02:35 +0000

    acabei definicao calssifica

--- aulo1.hs
+++ aulo1.hs
@@ -57,5 +57,7 @@ orderTriple (x, y, z) =
 
 classifica:: Int -> String
 classifica x = |x<=9 = reprovado
-               |x<= 10 && x>=12 = suficiente
-               |x<=13 && 
+               |x>= 10 && x<=12 = suficiente
+               |x>=13 && x<=15 = bom
+               |x>=16 && x<=18 = muito bom
+               |x=19 || x=20 = muito bom com distincao
